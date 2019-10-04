33a215367a05abef0d410e4f5135ad211e86aa0f
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-10-04 18:19:32 -0400

    First commit

---
+++ src/list.ts
@@ -0,0 +1,17 @@
+
+
+mod core::list;
+
+
+fn core::list::sum(list) => reduce(list, add, 0);
+fn core::list::product(list) => reduce(list, mul, 1);
+
+fn core::list::join(list) {
+	result = "";
+	
+	for _, elem in list {
+		result = result + format(elem);
+	}
+
+	result
+}
---
+++ src/math.ts
@@ -0,0 +1,44 @@
+
+
+
+mod core::math;
+
+core::math::PI = 3.141592653589;
+
+
+fn core::math::factorial(n) {
+	core::list::product(range(1, n))
+}
+
+
+fn core::math::pow(n, exponent) {
+	result = n;
+
+	for _, _ in range(0, exponent) {
+		result = result * n;
+	}
+
+	result
+}
+
+
+fn core::math::sin(angle) {
+	angle = angle % (2*core::math::PI);
+	angle
+	- (pow(angle, 3)/factorial(3))
+	+ (pow(angle, 5)/factorial(5))
+        - (pow(angle, 7)/factorial(7))
+}
+
+
+
+fn core::math::cos(angle) {
+        angle = angle % (2*core::math::PI);
+        1
+        - (pow(angle, 2)/factorial(2))
+        + (pow(angle, 4)/factorial(4))
+        - (pow(angle, 6)/factorial(6))
+}
+
+
+fn core::math::tan(angle) => sin(angle)/cos(angle);
