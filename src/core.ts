b54aca0bfaa82ead585e0055ffd21142043f02f1
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-10-09 17:25:40 -0400

    Up to date with newest tsar with foreign packages

--- src/core.ts
+++ src/core.ts
@@ -1,1 +1,22 @@
 mod core;
+
+new = @new;
+
+add = @add;
+sub = @sub;
+mul = @mul;
+div = @div;
+rem = @rem;
+not = @not;
+eq  = @eq;
+neq = @neq;
+
+list  = @list;
+len   = @len;
+push  = @push;
+pop   = @pop;
+range = @range;
+reverse = @reverse;
+map     = @map;
+filter  = @filter;
+reduce  = @reduce;
